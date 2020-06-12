403f00ed3791ae5817f295224bb4404a5c6bd767
Jaime Hidalgo Fernandez <jaimehidalgofernandez@gmail.com>
2020-06-12 18:51:09 +0200

    Add seeds

--- server/bin/seeds.js
+++ server/bin/seeds.js
@@ -12,4 +12,48 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
-  
+  let notes =[{
+    username:"Lorem",
+    note:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    fav:true,
+  },
+  {
+    username:"ipsum",
+    note:"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    fav:true,
+  },
+  {
+    username:"dolor",
+    note:"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    fav:false,
+  },
+  {
+    username:"sit",
+    note:"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    fav:true,
+  },
+  {
+    username:"amet",
+    note:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    fav:false,
+  },
+  {
+    username:"consectetur",
+    note:"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    fav:false,
+  }
+]
+
+
+Note.deleteMany()
+.then(()=> {
+    return Note.insertMany(notes);
+})
+.then(()=> {
+    console.log("succesfully added all the notes");
+    mongoose.connection.close();
+    process.exit(0);
+})
+.catch(err => {
+    console.error("Error connecting to mongo", err);
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router  = express.Router();
+const Note = require("../models/Note")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
 
 module.exports = router;
